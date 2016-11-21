a2fd4ca1ba86045a42bd1ca9a7f4dbdd7e385277
Anatoliy Arinovich <arinov@tut.by>
2016-11-21 16:25:55 +0300

    Jawsdb

--- config/database.js
+++ config/database.js
@@ -1,16 +1,16 @@
 // config/database.js
 module.exports = {
-    'connection': {
-        'host': '127.0.0.1',
-        'user': '12345qwert',
-        'password': '12345qwert',
-        'database': 'database1',
-     },//'connection': {
+    // 'connection': {
     //     'host': '127.0.0.1',
-    //     'user': 'folian_by',
-    //     'password': 'WD@oJ[TEF(6L',
-    //     'database': 'folian_1',
-    // },
-	'database': 'folian_1',
-    'users_table': 'users'
+    //     'user': '12345qwert',
+    //     'password': '12345qwert',
+    //     'database': 'database1',
+    //  },
+  'connection': {
+    'host': 'sulnwdk5uwjw1r2k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    'port': '3306',
+    'user': 'sp8zeeprknboqyu4',
+    'password': 'vbrna0ifpas2o3y5',
+    'database': 'ra3n7n9ejp7x9l3i',
+  },
 };
